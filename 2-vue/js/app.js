6bc0b81edb34e7170faaea49388b990bd9ab0c1b
eohyemin <eohyemin@gmail.com>
2020-04-17 03:38:25 +0900

    검색결과 구현 1

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,18 +1,27 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el: '#app',
     data: {
-        query: ''
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
-            
+            this.search();
         },
         onReset() {
             this.query = ''
-            debugger
         },
         onKeyup() {
             if(!this.query.length) this.onReset()
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
         }
     }
 })
